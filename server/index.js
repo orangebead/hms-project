f4135269ed628c0a4567ee13f7c7086176699952
orangebead <neil011205@gmail.com>
2026-05-04 14:44:00 +0400

    url issue fix trial-3.5 CORS

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://hms-project-2026.vercel.app/']
+  origin: ['http://localhost:5173', 'https://hms-project-2026.vercel.app']
 }));
 app.use(express.json());
 
